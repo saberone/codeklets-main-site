eee3eaf2e360fca9f0b5122d3a7b48881f68a15c
Saber Karmous <saberone@gmail.com>
2021-03-22 09:51:06 +0100

    Update dependencies

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -33,7 +33,7 @@ export default async (req, res) => {
     // 5. Swallow any errors from getrevue and return a better error message.
     if (response.status >= 400) {
       return res.status(400).json({
-        error: `Er ging wat mis tijdens het aanmelden voor de nieuwsbrief. Stuur een e-mail naar codeklets@protonmail en dan maken we het in orde.`,
+        error: `Er ging wat mis tijdens het aanmelden voor de nieuwsbrief. Stuur een e-mail naar codeklets@protonmail.com en dan maken we het in orde.`,
       });
     }
 
