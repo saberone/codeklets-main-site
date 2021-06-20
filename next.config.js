61d10a41a3b45e614b3a9fd9285103007b551581
Saber Karmous <saberone@gmail.com>
2021-06-20 21:07:59 +0200

    Upgrade to next.js 11
    Replace next-react-svg with svgr/webpack

--- next.config.js
+++ next.config.js
@@ -1,20 +1,35 @@
-const withPlugins = require("next-compose-plugins");
+/*const withPlugins = require("next-compose-plugins");
 const withReactSvg = require("next-react-svg");
 const path = require("path");
 
-/* module.exports = withPlugins([
+module.exports = withPlugins([
   withReactSvg,
   {
-    include: path.resolve(__dirname, 'assets/svg')
+    include: path.resolve(__dirname, "assets/svg"),
+    webpack(config, options) {
+      return config;
+    },
   },
-]);*/
-
-module.exports = withReactSvg({
-  future: {
-    webpack5: true,
-  },  
+]);
+*/
+/* module.exports = withReactSvg({
+  future: {},
   include: path.resolve(__dirname, "assets/svg"),
   webpack(config, options) {
     return config;
   },
-});
+});*/
+
+module.exports = {
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test(".svg")
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+    config.module.rules.push({
+      test: /\.svg$/,
+      loader: require.resolve("@svgr/webpack"),
+    });
+    return config;
+  },
+};
