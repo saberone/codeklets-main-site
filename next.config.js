94272d4c98d418655b2c6205db03addd9b4bee6c
Saber Karmous <saberone@gmail.com>
2021-09-06 10:48:13 +0200

    Replace next/mdx with xdm to fix vulnerability alert

--- next.config.js
+++ next.config.js
@@ -2,12 +2,23 @@ const withPlugins = require("next-compose-plugins");
 const withReactSvg = require("next-react-svg");
 const path = require("path");
 
-const withMDX = require("@next/mdx")({
+/*const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
-});
+}); */
 
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
+  // Support MDX files as pages:
+  pageExtensions: ["mdx", "tsx", "ts", "jsx", "js"],
+
   webpack(config, options) {
+    config.module.rules.push({
+      test: /\.mdx$/,
+      use: [{ loader: "xdm/webpack.cjs", options: {} }],
+    });
+
     return config;
   },
 };
@@ -18,12 +29,6 @@ module.exports = withPlugins(
       withReactSvg,
       {
         include: path.resolve(__dirname, "assets/svg"),
-      },
-    ],
-    [
-      withMDX,
-      {
-        pageExtensions: ["js", "jsx", "mdx"],
       },
     ],
   ],
