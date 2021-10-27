0bed6660fcc5d0022025e670ac601cd034fac4b7
Saber Karmous <saberone@gmail.com>
2021-10-27 21:35:21 +0200

    [WIP] About us page

---
+++ data/team.js
@@ -0,0 +1,56 @@
+const team = [
+  {
+    fullName: "Bernard Kroes",
+    role: "Co-host",
+    imageSrc: "/images/team/bernard_400x400.jpg",
+    socials: {
+      linkedIn: "https://www.linkedin.com/in/bernard-kroes-5050a82/",
+      github: "https://github.com/bernardkroes",
+    },
+  },
+  {
+    fullName: "Johnny Dongelmans",
+    role: "Co-host",
+    story:
+      "Ruby developer, doet onder andere dingen met beveiliging en online identificatie.",
+    imageSrc: "/images/team/johnny_400x400.jpg",
+    socials: {
+      linkedIn: "https://www.linkedin.com/in/johnnydongelmans/",
+      github: "https://github.com/jdongelmans",
+    },
+  },
+  {
+    fullName: "Pauline Vos",
+    role: "Co-host",
+    imageSrc: "/images/team/pauline_400x400.jpg",
+    socials: {
+      linkedIn: "https://www.linkedin.com/in/paulinepvos/",
+      github: "https://github.com/paulinevos",
+      twitter: "https://twitter.com/vanamerongen",
+      personal: "https://www.pauline-vos.nl",
+    },
+  },
+  {
+    fullName: "Kishen Simbhoedatpanday",
+    role: "Host",
+    imageSrc: "/images/team/kishen_400x400.jpg",
+    socials: {
+      linkedIn: "https://www.linkedin.com/in/kishensimbhoedatpanday/",
+      twitter: "https://twitter.com/kishenpanday",
+    },
+  },
+  {
+    fullName: "Saber Karmous",
+    role: "Host",
+    imageSrc: "/images/team/saber_400x400.jpg",
+    story:
+      "Software Artist, omdat hij niet gespecialiseerd is in één aspect van software ontwikkeling. Programmeert in C#, Javascript/Typescript en vind zowel front-end als back-end leuk. Verder heeft hij ook de rol van solution architect mogen invullen. Maar vindt eigenlijk teveel leuk.",
+    socials: {
+      linkedIn: "https://www.linkedin.com/in/saberkarmous/",
+      github: "https://github.com/saberone",
+      twitter: "https://twitter.com/sdotone",
+    },
+  },
+];
+
+export default team;
