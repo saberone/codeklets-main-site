a8d4d4388f8ae11ec1f87b27f4d749f5083cca39
Saber Karmous <saberone@gmail.com>
2021-11-06 15:21:15 +0100

    Update Bernard's story

--- data/team.js
+++ data/team.js
@@ -3,12 +3,12 @@ const team = [
     fullName: "Bernard Kroes",
     role: "Co-host",
     story:
-      "Programmeert al veel te lang, tegenwoordig niet meer in GFA Basic, maar vooral in Swift, Objective-C en Ruby.",
+      "Van GFA-Basic toen tot Ruby, Swift en Objective-C nu. Old school. Pragmatisch. It never hurts to help!",
     imageSrc: "/images/team/bernard_400x400.jpg",
     socials: {
       linkedIn: "https://www.linkedin.com/in/bernard-kroes-5050a82/",
-      github: "https://github.com/bernardkroes",
-    },
+      github: "https://github.com/bernardkroes"
+    }
   },
   {
     fullName: "Johnny Dongelmans",
@@ -18,8 +18,8 @@ const team = [
     imageSrc: "/images/team/johnny_400x400.jpg",
     socials: {
       linkedIn: "https://www.linkedin.com/in/johnnydongelmans/",
-      github: "https://github.com/jdongelmans",
-    },
+      github: "https://github.com/jdongelmans"
+    }
   },
   {
     fullName: "Pauline Vos",
@@ -29,8 +29,8 @@ const team = [
       linkedIn: "https://www.linkedin.com/in/paulinepvos/",
       github: "https://github.com/paulinevos",
       twitter: "https://twitter.com/vanamerongen",
-      personal: "https://www.pauline-vos.nl",
-    },
+      personal: "https://www.pauline-vos.nl"
+    }
   },
   {
     fullName: "Kishen Simbhoedatpanday",
@@ -38,8 +38,8 @@ const team = [
     imageSrc: "/images/team/kishen_400x400.jpg",
     socials: {
       linkedIn: "https://www.linkedin.com/in/kishensimbhoedatpanday/",
-      twitter: "https://twitter.com/kishenpanday",
-    },
+      twitter: "https://twitter.com/kishenpanday"
+    }
   },
   {
     fullName: "Saber Karmous",
@@ -50,9 +50,9 @@ const team = [
     socials: {
       linkedIn: "https://www.linkedin.com/in/saberkarmous/",
       github: "https://github.com/saberone",
-      twitter: "https://twitter.com/sdotone",
-    },
-  },
+      twitter: "https://twitter.com/sdotone"
+    }
+  }
 ];
 
 export default team;
