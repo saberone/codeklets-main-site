422a5cf04dac74703b49378a69ca21df4708d03a
Saber Karmous <saberone@gmail.com>
2022-01-18 23:01:03 +0100

    Update security headers

--- next.config.js
+++ next.config.js
@@ -2,9 +2,32 @@ const withPlugins = require("next-compose-plugins");
 const withReactSvg = require("next-react-svg");
 const path = require("path");
 
-/*const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-}); */
+const securityHeaders = [
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: "'default-src 'self'",
+  },
+];
 
 /**
  * @type {import('next').NextConfig}
@@ -12,6 +35,15 @@ const path = require("path");
 const nextConfig = {
   // Support MDX files as pages:
   pageExtensions: ["mdx", "tsx", "ts", "jsx", "js"],
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
 
   webpack(config, options) {
     config.module.rules.push({
@@ -34,9 +66,3 @@ module.exports = withPlugins(
   ],
   nextConfig
 );
-
-/* module.exports = withReactSvg({
-    
-  include: path.resolve(__dirname, "assets/svg"),
-  
-}); */
