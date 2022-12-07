f6e0f3857750fd9702ecfb2842389d0d0c160789
Saber Karmous <saberone@gmail.com>
2022-12-07 11:46:35 +0100

    Upgrade to next 13

--- components/post-preview.js
+++ components/post-preview.js
@@ -14,8 +14,12 @@ export default function PostPreview({
   return (
     <div>
       <h3 className="text-3xl mb-3 leading-snug font-bold text-gray-600 tracking-tighter">
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a className="hover:underline">{title}</a>
+        <Link
+          as={`/posts/${slug}`}
+          href="/posts/[slug]"
+          className="hover:underline"
+        >
+          {title}
         </Link>
       </h3>
       <div className="mb-4 text-base leading-6 font-medium">
